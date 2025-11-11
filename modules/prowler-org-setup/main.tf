84671c3cd0687c6bbb5bcb957af20d6cc513ce1b
vibhoragarwal81 <79957273+vibhoragarwal81@users.noreply.github.com>
2025-11-11 18:01:00 +0530

    Update main.tf

--- modules/prowler-org-setup/main.tf
+++ modules/prowler-org-setup/main.tf
@@ -1,14 +1,19 @@
 
 variable "region" { default = "us-east-1" }
 variable "results_bucket_name" {}
-variable "github_repo" {} # e.g., vibhoragarwal81/prowler-testing
-variable "org_account_ids" { type = list(string) }
+variable "github_repo" {} # e.g., vibhor/prowler-org-scanner
 variable "management_account_id" {}
 
 provider "aws" {
   region = var.region
 }
 
+# Fetch all accounts in the organization
+data "aws_organizations_organization" "org" {}
+
+data "aws_organizations_accounts" "accounts" {}
+
+# Create S3 bucket for Prowler results
 resource "aws_s3_bucket" "prowler_results" {
   bucket = var.results_bucket_name
   force_destroy = true
@@ -19,18 +24,15 @@ resource "aws_s3_bucket" "prowler_results" {
   }
 }
 
+# GitHub OIDC provider
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98c6a5d7b5a0e3b1a1f2b5e3e1b3c2f3"
-  ]
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98c6a5d7b5a0e3b1a1f2b5e3e1b3c2f3"]
 }
 
+# GitHubProwlerRunner role in management account
 resource "aws_iam_role" "github_prowler_runner" {
   name = "GitHubProwlerRunner"
 
@@ -45,8 +47,7 @@ resource "aws_iam_role" "github_prowler_runner" {
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*",
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
           }
         }
       }
@@ -64,36 +65,38 @@ resource "aws_iam_role_policy" "github_prowler_runner_policy" {
         Effect = "Allow",
         Action = [
           "sts:AssumeRole",
+          "organizations:ListAccounts",
           "iam:CreateRole",
           "iam:PutRolePolicy",
-          "iam:AttachRolePolicy",
-          "organizations:ListAccounts"
+          "iam:AttachRolePolicy"
         ],
         Resource = "*"
       },
       {
         Effect = "Allow",
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          aws_s3_bucket.prowler_results.arn,
-          "${aws_s3_bucket.prowler_results.arn}/*"
-        ]
+        Action = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"],
+        Resource = [aws_s3_bucket.prowler_results.arn, "${aws_s3_bucket.prowler_results.arn}/*"]
       }
     ]
   })
 }
 
-# Create ProwlerAuditRole in each member account
+# Configure provider aliases for each member account
+provider "aws" {
+  alias  = "member"
+  region = var.region
+  assume_role {
+    role_arn = "arn:aws:iam::${var.management_account_id}:role/OrganizationAccountAccessRole"
+  }
+}
+
+# Create ProwlerAuditRole in each member account dynamically
 resource "aws_iam_role" "prowler_audit_role" {
-  for_each = toset(var.org_account_ids)
+  for_each = { for acc in data.aws_organizations_accounts.accounts.accounts : acc.id => acc }
 
-  provider = aws
+  provider = aws.member
 
-  name = "ProwlerAuditRole-${each.key}"
+  name = "ProwlerAuditRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -110,9 +113,11 @@ resource "aws_iam_role" "prowler_audit_role" {
 }
 
 resource "aws_iam_role_policy" "prowler_audit_policy" {
-  for_each = toset(var.org_account_ids)
+  for_each = aws_iam_role.prowler_audit_role
 
-  role = aws_iam_role.prowler_audit_role[each.key].name
+  provider = aws.member
+
+  role = each.value.name
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -128,7 +133,6 @@ resource "aws_iam_role_policy" "prowler_audit_policy" {
           "iam:GenerateCredentialReport",
           "iam:Get*",
           "iam:List*",
-          "organizations:ListAccounts",
           "s3:ListAllMyBuckets",
           "s3:GetBucketLocation",
           "s3:ListBucket",
